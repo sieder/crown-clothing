fcbb0644267cc2748b1a859b625d0d42cc3e2693
sieder <sieder00@gmail.com>
2019-09-02 22:00:41 +0300

    updating our app with a shop page, collection preview component and collection item component

--- src/App.js
+++ src/App.js
@@ -1,38 +1,19 @@
 import React from 'react';
 import './App.css';
-import { Route } from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
 
 import HomePage from './pages/homepage/homepage.components'
-
-const HatsPage = () => (
-  <div>
-    <h1>HATS PAGE</h1>
-    
-  </div>
-)
-
-const TopicDetails = ( props ) => {
-  console.log(props)
-  console.log(props.match.params.topicID)
-  return (
-    <div>
-      <h1>Topic Details</h1>
-      {
-        props.match.params.topicID === "1" ? <h1>{props.match.params.topicID}</h1> : <h1>NONE</h1> 
-      }
-      <button onClick={() => props.history.push('/')}>back</button>
-    </div>
-  )
-}
+import ShopPage from './pages/shop/shop.component'
 
 function App() {
   return (
     <div>
-      <Route exact path='/' component={HomePage} />
-      <Route path='/hats' component={HatsPage} />
-      <Route path='/topicdetails/:topicID' component={TopicDetails} />
+      <Switch>
+        <Route exact path='/' component={HomePage} />
+        <Route path='/shop' component={ShopPage} />
+      </Switch>
     </div>
   )
 }
 
-export default App;
+export default App
--- src/pages/homepage/homepage.components.jsx
+++ src/pages/homepage/homepage.components.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 
 import Directory from '../../components/directory/directory.components'
+
 import './homepage.styles.scss'
 
 const HomePage = () => (
