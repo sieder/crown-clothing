39db66e85f2365d24c401186c318eed9f8bcdb2d
sieder <sieder00@gmail.com>
2019-10-06 12:58:02 +0300

    Updates

---
+++ precache-manifest.ea4556e7a6f6523e3143c04b010b2295.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c324cfa71a7c6f95ccd7a657c8500e0d",
+    "url": "/crown-clothing/index.html"
+  },
+  {
+    "revision": "55d347a073d25bc31d6b",
+    "url": "/crown-clothing/static/css/main.9f0be8ff.chunk.css"
+  },
+  {
+    "revision": "8702c9d655d0ed855797",
+    "url": "/crown-clothing/static/js/2.887c8ada.chunk.js"
+  },
+  {
+    "revision": "55d347a073d25bc31d6b",
+    "url": "/crown-clothing/static/js/main.1378ff82.chunk.js"
+  },
+  {
+    "revision": "4f44eabe3fe1e07ac973",
+    "url": "/crown-clothing/static/js/runtime~main.ea7231f1.js"
+  },
+  {
+    "revision": "ed28accb7dd58a0469bff983503ff07e",
+    "url": "/crown-clothing/static/media/crown.ed28accb.svg"
+  }
+]);
